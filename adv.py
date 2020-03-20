01456b5f6536b0289387b06d3c45d8a61c479314
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-03-20 11:21:45 +0100

    Implement the traversal method

--- adv.py
+++ adv.py
@@ -49,11 +49,48 @@ def get_unseen_room(room_id, seen_rooms):
             seen_rooms.add(room)
             return room
 
+def re_explore(current_room, seen_rooms):
+    visited = []
+    paths = {}
+    q = Queue()
+    q.enqueue(current_room)
+    paths[current_room] = [current_room]
+    while q.size() > 0:
+        room = q.dequeue()
+        visited.append(room)
+        for searched_room in room_graph[room][1].values():
+            if searched_room in visited:
+                continue
+            new_path = paths[room].copy()
+            new_path.append(searched_room)
+            paths[searched_room] = new_path
+            if not has_seen_rooms(searched_room, seen_rooms):
+                un_explored_path = paths[searched_room]
+                directions = []
+                for i in range(len(un_explored_path) -1):
+                    for direction, value in room_graph[un_explored_path[i]][1].items():
+                        if value == un_explored_path[i + 1]:
+                            directions.append(direction)
+                    seen_rooms.add(un_explored_path[i + 1])
+                return (directions, un_explored_path[len(un_explored_path) - 1])
+            q.enqueue(searched_room)
+    return None
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = ['n', 's', 'e', 'w']
 
-
+seen_rooms = set()
+current_room = 0
+while True:
+    while not has_seen_rooms(current_room,seen_rooms):
+        current_room = get_unseen_room(current_room,seen_rooms)
+    explored_path = re_explore(current_room, seen_rooms)
+    if explored_path:
+        new_path = explored_path[0]
+        traversal_path.extend(new_path)
+        current_room = explored_path[1]
+    else:
+        break
 
 # TRAVERSAL TEST
 visited_rooms = set()
